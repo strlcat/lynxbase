bdc98a19c34c8ea170beeb2d39fbbcd1887187c0
Andrey Rys <rys@lynxlynx.ru>
2021-10-24 18:48:17 +0200

    null: fix stdin.

--- null.c
+++ null.c
@@ -2,17 +2,27 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
 	int fd;
+	int close_stdin = 0;
 
 	if (argc < 2) return 0;
 
+	if (!strcmp(argv[1], "-0")) {
+		argc--;
+		argv++;
+		close_stdin = 1;
+	}
+
 	fd = open("/dev/null", O_RDWR);
 	if (fd == -1) return 1;
-	if (close(0) == -1) return 2;
-	if (dup2(fd, 0) == -1) return 2;
+	if (close_stdin) {
+		if (close(0) == -1) return 2;
+		if (dup2(fd, 0) == -1) return 2;
+	}
 	if (close(1) == -1) return 2;
 	if (dup2(fd, 1) == -1) return 2;
 	if (close(2) == -1) return 2;
